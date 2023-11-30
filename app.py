782cbdcc213d02064ef50666c8e214023231b82a
davidmsf <davidmsf@gmail.com>
2023-11-30 16:39:27 +0100

    edited update endpoint

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 import uuid
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 from DBConnection import connection
 from PredictImage import PredictImage
 from PredictSniffingpoint import PredictSniffingpoint
@@ -60,38 +64,47 @@ def get_all():
         conn.close()
 
 
-@app.route('/update_testobject', methods=['POST'])
+@app.route('/update_testobject', methods=['PUT'])
 def update_testobject():
     try:
         conn = connection()
         cursor = conn.cursor()
         data = request.get_json(force=True)
+        print(data)
         # Insert into TestObjects
         cursor.execute('UPDATE TestObjects SET type = ?, serialNr = ?, imagePath = ? WHERE id = ?',
                        (data.get('type'), data.get('serialNr'), data.get('imagePath'), data.get('id')))
-        # Insert into SniffingPoint
+
+        # Check if the SniffingPoint already exists
+        cursor.execute('SELECT id FROM SniffingPoint WHERE testObjectId = ?', (data.get('id'),))
+        db_sniffing_points = [row[0] for row in cursor.fetchall()]
         sniffing_points = data.get('sniffingPoints')
         if sniffing_points:
+            existing_sniffing_points = []
             for point in sniffing_points:
-                # Check if the SniffingPoint already exists
-                cursor.execute('SELECT id FROM SniffingPoint WHERE id = ?', (point.get('id'),))
-                exists = cursor.fetchone()
-
-                if exists:
+                if point.get('id') in db_sniffing_points:
                     # Update existing SniffingPoint
                     cursor.execute('UPDATE SniffingPoint SET name = ?, x = ?, y = ? WHERE id = ?',
                                    (point.get('name'), point.get('x'), point.get('y'), point.get('id')))
+                    print("updating", point.get('name'))
+                    existing_sniffing_points.append(point.get('id'))
                 else:
-                    new_id = str(uuid.uuid4())  # Generate a new GUID
                     # Insert new SniffingPoint
                     cursor.execute('INSERT INTO SniffingPoint (name, x, y, testObjectId) VALUES (?, ?, ?, ?)',
-                                   (point.get('name'), point.get('x'), point.get('y'), new_id))
+                                   (point.get('name'), point.get('x'), point.get('y'), data.get('id')))
+                    print("creating", point.get('name'))
+
+            for db_point in db_sniffing_points:
+                if db_point not in existing_sniffing_points:
+                    print("deleting", db_point)
+                    cursor.execute('DELETE FROM Sniffingpoint WHERE id = ?', (db_point,))
 
         conn.commit()  # Commit the transaction
         return jsonify({"message": "TestObject added successfully"}), 201
 
     except Exception as e:
         conn.rollback()  # Rollback in case of error
+        logging.exception("An error occurred during update:")  # Logs the exception with traceback
         return jsonify({"error": str(e)}), 500
 
     finally:
@@ -117,12 +130,52 @@ def create_testobject():
                                (point.get('name'), point.get('x'), point.get('y'), new_id))
 
         conn.commit()  # Commit the transaction
+        return jsonify({"id": new_id}), 201
+
+    except Exception as e:
+        conn.rollback()  # Rollback in case of error
+        return jsonify({"error": str(e)}), 500
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/edit_testresult', methods=['POST'])
+def edit_testresult():
+    try:
+        conn = connection()
+        cursor = conn.cursor()
+        data = request.get_json(force=True)
+        # Update Sniffingpoint
+        cursor.execute('UPDATE SniffingPoint SET leak = ?, reason = ? WHERE id = ?',
+                       (data.get('leak'), data.get('reason'), data.get('id')))
+
+        conn.commit()  # Commit the transaction
         return jsonify({"message": "TestObject added successfully"}), 201
 
     except Exception as e:
         conn.rollback()  # Rollback in case of error
         return jsonify({"error": str(e)}), 500
 
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/delete/<id>', methods=['DELETE'])
+def delete(id):
+    if not id:
+        return jsonify({"error": "Missing id parameter"}), 400
+    try:
+        conn = connection()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM Sniffingpoint WHERE testObjectId = ?', (id,))
+        cursor.execute('DELETE FROM TestObjects WHERE id = ?', (id,))
+        conn.commit()  # Commit the transaction
+        return jsonify({"message": "TestObject deleted successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
     finally:
         cursor.close()
         conn.close()
@@ -186,10 +239,54 @@ def get_testobject_with_results(id):
         conn.close()
 
 
+@app.route('/get_testobject_with_sniffingpoints/<id>', methods=['GET'])
+def get_testobject_with_sniffingpoints(id):
+    if not id:
+        return jsonify({"error": "Missing id parameter"}), 400
+
+    try:
+        conn = connection()
+        cursor = conn.cursor()
+
+        # Fetch TestObject
+        cursor.execute('SELECT * FROM TestObjects WHERE Id = ?', (id,))
+        test_object_columns = [column[0] for column in cursor.description]
+        test_object_data = [dict(zip(test_object_columns, row)) for row in cursor.fetchall()]
+
+        if not test_object_data:
+            return jsonify({"error": "TestObject not found"}), 404
+
+        # Fetch associated SniffingPoints
+        cursor.execute('SELECT id, testObjectId, name, x, y FROM SniffingPoint WHERE TestObjectId = ?', (id,))
+        sniffing_point_columns = [column[0] for column in cursor.description]
+        sniffing_points_data = [dict(zip(sniffing_point_columns, row)) for row in cursor.fetchall()]
+
+        # Combine data
+        combined_data = {
+            "TestObject": test_object_data[0],
+            "SniffingPoints": sniffing_points_data
+        }
+
+        return jsonify(combined_data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        cursor.close()
+        conn.close()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
+#         if point:
+#             # Update existing SniffingPoint
+#             cursor.execute('UPDATE SniffingPoint SET name = ?, x = ?, y = ? WHERE id = ?',
+#                            (point.get('name'), point.get('x'), point.get('y'), point.get('id')))
+#         else:
+#             # Insert new SniffingPoint
+#             cursor.execute('INSERT INTO SniffingPoint (name, x, y, testObjectId) VALUES (?, ?, ?, ?)',
+#                            (point.get('name'), point.get('x'), point.get('y'), data.get('id')))
+#
 
 #
 # class PredictSniffingpoint:
